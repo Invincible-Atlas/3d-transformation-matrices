a390eef1c254edcaa98f0da3c360e6ee96ecf36a
Invincible-Atlas <148077776+Invincible-Atlas@users.noreply.github.com>
2025-02-21 09:02:47 -0500

    Added ortho function

--- index.js
+++ index.js
@@ -1,47 +1,49 @@
-const transMatrix = (function(){
+const transMatrix = (function () {
     let pub = {};
-    pub.translate = function(x,y,z){
-        return([[1,0,0,0],[0,1,0,0],[0,0,1,0],[x,y,z,1]]);
+    pub.translate = function (x, y, z) {
+        return ([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [x, y, z, 1]]);
     }
-    pub.rotateX = function(theta){
+    pub.rotateX = function (theta) {
         let c = Math.cos(theta);
         let s = Math.sin(theta);
-        return([[1,0,0,0],[0,c,s,0],[0,-s,c,0],[0,0,0,1]]);
+        return ([[1, 0, 0, 0], [0, c, s, 0], [0, -s, c, 0], [0, 0, 0, 1]]);
     }
-    pub.rotateY = function(theta){
+    pub.rotateY = function (theta) {
         let c = Math.cos(theta);
         let s = Math.sin(theta);
-        return([[c,0,-s,0],[0,1,0,0],[s,0,c,0],[0,0,0,1]]);
+        return ([[c, 0, -s, 0], [0, 1, 0, 0], [s, 0, c, 0], [0, 0, 0, 1]]);
     }
-    pub.rotateZ = function(theta){
+    pub.rotateZ = function (theta) {
         let c = Math.cos(theta);
         let s = Math.sin(theta);
-        return([[c,s,0,0],[-c,s,0,0],[0,0,1,0],[0,0,0,1]]);
+        return ([[c, s, 0, 0], [-c, s, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]]);
     }
-    pub.scale = function(x,y,z){
-        return([[x,0,0,0],[0,y,0,0],[0,0,z,0],[0,0,0,1]]);
+    pub.scale = function (x, y, z) {
+        return ([[x, 0, 0, 0], [0, y, 0, 0], [0, 0, z, 0], [0, 0, 0, 1]]);
     }
-    pub.shearX = function(y,z){
-        return([[1,y,z,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]])
+    pub.shearX = function (y, z) {
+        return ([[1, y, z, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
     }
-    pub.shearY = function(x,z){
-        return([[1,0,0,0],[x,1,z,0],[0,0,1,0],[0,0,0,1]])
+    pub.shearY = function (x, z) {
+        return ([[1, 0, 0, 0], [x, 1, z, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
     }
-    pub.shearZ = function(y,z){
-        return([[1,0,0,0],[0,1,0,0],[y,z,1,0],[0,0,0,1]])
+    pub.shearZ = function (y, z) {
+        return ([[1, 0, 0, 0], [0, 1, 0, 0], [y, z, 1, 0], [0, 0, 0, 1]])
     }
-    pub.reflectXY = function(){
-        return([[1,0,0,0],[0,1,0,0],[0,0,-1,0],[0,0,0,1]])
+    pub.reflectXY = function () {
+        return ([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
     }
-    pub.reflectYZ = function(){
-        return([[-1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]])
-    }   
-    pub.reflectZX = function(){
-        return([[1,0,0,0],[0,-1,0,0],[0,0,1,0],[0,0,0,1]])
-    } 
-    pub.identity = function(){
-        return([[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]])
+    pub.reflectYZ = function () {
+        return ([[-1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
     }
-    
-    return(pub)
+    pub.reflectZX = function () {
+        return ([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
+    }
+    pub.identity = function () {
+        return ([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
+    }
+    pub.ortho = function (left, right, bottom, top, near, far) {
+        return [[2 / (right - left), 0, 0, 0], [0, 2 / (top - bottom), 0, 0], [0, 0, 2 / (near - far), 0], [(left + right) / (left - right), (bottom + top) / (bottom - top), (near + far) / (near - far), 1],];
+    }
+    return (pub)
 })();
